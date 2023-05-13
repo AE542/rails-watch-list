ffb709b64adbd26353380b5672ee4bf1301532f1
Mohammed Qureshi <mqureshi93v2@gmail.com>
2023-05-13 11:48:13 +0100

    Added extra gems

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,20 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "lists#index"
 
   # movies
    get "movies", to: "movies#index"
 
   # lists
-  resources :lists do
-    resources :bookmarks
+  resources :lists, except: [:edit, :update] do
+    # this is how we can only allow the user to create and delete their lists only
+    resources :bookmarks, only: [:new, :create]
+    resources :reviews, only: :create
   end
+
+  resources :bookmarks, only: :destroy
+  resources :reviews, only: :destroy
 
   # get "lists", to: "lists#index"
 
